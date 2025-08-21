c7b30d16e4a747cb97ce3df2edf1481a02b5921a
merytpeters <merytpeters@gmail.com>
2025-08-21 01:49:14 +0100

    Add start script for database migrations and application startup

---
+++ server-app/start.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo "🔄 Running database migrations..."
+npx prisma migrate deploy
+echo "🌱 Seeding database..."
+npm run prod:db:seed
+echo "🚀 Starting application..."
+npm run start:dev
